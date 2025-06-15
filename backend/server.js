79c90c51894ae67c6dc1c266ba3ed748693c0602
ThantPyae <thantpyaes01@gmail.com>
2025-06-15 21:26:56 +0630

    database prisma setup

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import cors from 'cors';
-import { ENV } from './config/env.js';
-import router from './router.js';
-import morgan from 'morgan';
+import express from "express";
+import cors from "cors";
+import { ENV } from "./config/env.js";
+import router from "./router.js";
+import morgan from "morgan";
 
 const app = express();
 const PORT = ENV.PORT;
@@ -10,16 +10,15 @@ const PORT = ENV.PORT;
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan('dev'));
-app.use(express.static('public'));
+app.use(morgan("dev"));
+app.use(express.static("public"));
 
-app.use('/api/v1', router);
+app.use("/api/v1", router);
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-    
-})
+  console.log(`Server running on http://localhost:${PORT}`);
+});
